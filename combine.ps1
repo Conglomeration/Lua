bbb433ccb71b17566c70d83aba028a8a48d65f13
MokiyCodes <MokiyCodes@wafflepowered.com>
2021-10-28 16:54:46 +0200

    Stuff

---
+++ combine.ps1
@@ -0,0 +1,18 @@
+Write-Output "Combine.ps1"
+Write-Output "Combines all Conglomeration-related Lua Files into one final output file."
+Write-Output "Copyright (c) 2021 Mokiy. See https://astolfodev.mit-license.org/ for Licensing Information."
+Write-Output ""
+Write-Output "@depends LuaSrcDiet"
+Write-Output "@depends LuaMin"
+Write-Output "@depends LuaCC"
+Write-Output "@depends NodeJS >= 16"
+Write-Output ""
+Set-Location src;
+cmd /c luacc -o ../dist/out-tmp.lua Conglomeration util Parsers Base64 typeof;
+Set-Location ../dist;
+cmd /c node combine-fixtmp.js;
+cmd /c LuaSrcDiet out-tmp.lua -o out-tmp2.lua --opt-entropy --opt-comments --opt-whitespace --opt-emptylines --opt-eols --opt-strings --opt-numbers --opt-locals --details > luasrcdiet.log;
+cmd /c luamin -f out-tmp2.lua > out.lua;
+Set-Location ..
+Write-Output "Done!"
+Write-Output ""
---
+++ combine.sh
@@ -0,0 +1,17 @@
+echo ""
+echo "Combine.sh"
+echo "Combines all Conglomeration-related Lua Files into one final output file."
+echo "Copyright (c) 2021 Mokiy. See https://astolfodev.mit-license.org/ for Licensing Information."
+echo ""
+echo "@depends LuaSrcDiet"
+echo "@depends LuaMin"
+echo "@depends LuaCC"
+echo "@depends NodeJS >= 16"
+echo ""
+cd src;
+luacc -o ../dist/out-tmp.lua Conglomeration util Parsers Base64 typeof;
+cd ../dist;
+node combine-fixtmp.js;
+LuaSrcDiet out-tmp.lua -o out-tmp2.lua --opt-entropy --opt-comments --opt-whitespace --opt-emptylines --opt-eols --opt-strings --opt-numbers --opt-locals --details > luasrcdiet.log;
+luamin -f out-tmp2.lua > out.lua;
+echo "Done!"
